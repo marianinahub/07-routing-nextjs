e77cc017f7dfbe69b76dc0930ccf02a2d79b4343
Nina Aksenich <maruaninashved1488@gmail.com>
2026-04-24 16:28:50 +0300

    homework

---
+++ lib/api.ts
@@ -0,0 +1,53 @@
+import axios from "axios";
+import type { Note, NoteTag } from "@/types/note";
+
+export interface FetchNotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export interface CreateNoteProps {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+const START_URL = "https://notehub-public.goit.study/api";
+const TOKEN = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+const noteHubApi = axios.create({
+  baseURL: START_URL,
+  headers: {
+    Authorization: `Bearer ${TOKEN}`,
+  },
+});
+
+export const fetchNotes = async (
+  page: number,
+  search = "",
+): Promise<FetchNotesResponse> => {
+  const response = await noteHubApi.get<FetchNotesResponse>("/notes", {
+    params: {
+      page,
+      perPage: 12,
+      search,
+    },
+  });
+
+  return response.data;
+};
+
+export const fetchNoteById = async (id: string): Promise<Note> => {
+  const response = await noteHubApi.get<Note>(`/notes/${id}`);
+  return response.data;
+};
+
+export const createNote = async (newNote: CreateNoteProps): Promise<Note> => {
+  const response = await noteHubApi.post<Note>("/notes", newNote);
+  return response.data;
+};
+
+export const deleteNote = async (id: string): Promise<Note> => {
+  const response = await noteHubApi.delete<Note>(`/notes/${id}`);
+  return response.data;
+};
